509d2dfc9586b9a98f5ede85bc401fe21bd5f494
Aleksey Belikov <aabelikoff@gmail.com>
2023-01-14 12:20:14 +0300

    improvement complete

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,47 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent {
-  EASY_PATTERN = /(^\p{L}+$)|(^\p{N}+$)|(^[^\p{L}\p{N}]+$)/u
-  MEDIUM_PATTERN = /(^[\p{L}\p{N}]+$)|(^[^\p{L}]+$)|(^[^\p{N}]+$)/u
-  STRONG_PATTERN = /(\p{L}+\p{N}+[^\p{L}\p{N}]+)/u
-  
-  // password = {
-  //   length: '',
-  //   strength: ''
-  // }
-  
-  length = ''
-  strength = ''
 
-  defineStrength(value:string) {
-    if (value.match(this.EASY_PATTERN))
-      return 'easy'
-    if (value.match(this.MEDIUM_PATTERN))
-      return 'medium'
-    if (value.match(this.STRONG_PATTERN))
-      return 'strong'
-    return ''
+export class FormComponent implements OnInit {
+  patterns = [/\p{L}/u,/\p{N}/u,/[^\p{L}\p{N}]/u]
+  
+  length: string = ''
+  strength: number = 0
+  show: boolean = false
+  
+  ngOnInit() {
+    
   }
 
-  defineLength(value: string) {
+  setShow() {
+    this.show = !this.show
+  }
+
+  setStrength(value: string): number {
+   return this.patterns.reduce((counter, pattern) => {
+      if (value.match(pattern)) 
+        return ++counter
+      return counter
+    }, 0)
+  }
+
+  setLength(value: string): string {
     if (!value.length) 
       return 'empty'
     if (value.length >= 8)
-      return 'strong'
-    return 'easy'
+      return 'full'
+    return 'weak'
   }
   
-  validatePassword(value: any) {
-    this.length = this.defineLength(value)
-    this.strength = this.defineStrength(value)
-    console.log(this.length + '  ' + this.strength);
-    console.log(value)
-    console.log(value.match(this.STRONG_PATTERN))
+  validatePassword(value: string) {
+    this.length=this.setLength(value)
+    this.strength=this.setStrength(value)
   }
 
 }
